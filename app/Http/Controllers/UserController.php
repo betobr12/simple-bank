08c6694663d44ace1af21c31e88b395ac014088e
Roberto <roberto.rodrigues@qualityinteligencia.com.br>
2024-03-26 13:42:06 -0300

    new architecture test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,110 +2,117 @@
 
 namespace App\Http\Controllers;
 
-use App\Libraries\Control;
-use App\Libraries\Document;
 use App\User;
+use App\Libraries\Document;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
-
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
+    /**
+     * @param Request $request
+     */
     protected function login(Request $request)
     {
-        $cpf_cnpj = preg_replace( '/[^0-9]/', '', $request->cpf);
+        $cpf_cnpj = preg_replace('/[^0-9]/', '', $request->cpf);
 
-        $document           = new Document;
+        $document = new Document;
         $document->cpf_cnpj = $cpf_cnpj;
 
         if ($document->validateCPF() == false) {
-            return response()->json(array("error"=>"CPF invalido"));
+            return response()->json(["error" => "CPF invalido"]);
         }
 
-        $validator = Validator::make($request->all(),[
-            'password' => ['required', 'string'],
-        ],[
-            'password.required'     => 'Senha obrigatória. ',
+        $validator = Validator::make($request->all(), [
+            'password' => ['required', 'string']
+        ], [
+            'password.required' => 'Senha obrigatória. '
         ]);
 
         if ($validator->fails()) {
-            return response()->json(array("error"=>$validator->errors()->first()));
+            return response()->json(["error" => $validator->errors()->first()]);
         }
 
         if (Auth::attempt(['cpf' => $cpf_cnpj, 'password' => $request->password])) {
             $user = auth()->user();
             $user->token = $user->createToken($cpf_cnpj)->accessToken;
-            return response()->json(array("success" => "Usuário logado com sucesso", "user" => $user));
-        } 
-        return response()->json(array("error" => "Usuário ou senha invalido"));                
+            return response()->json(["success" => "Usuário logado com sucesso", "user" => $user]);
+        }
+        return response()->json(["error" => "Usuário ou senha invalido"]);
     }
 
+    /**
+     * @param Request $request
+     */
     protected function register(Request $request)
     {
-        $cpf_cnpj = preg_replace( '/[^0-9]/', '', $request->cpf);
+        $cpf_cnpj = preg_replace('/[^0-9]/', '', $request->cpf);
 
-        $document           = new Document;
+        $document = new Document;
         $document->cpf_cnpj = $cpf_cnpj;
 
         if ($document->validateCPF() == false) {
-            return response()->json(array("error"=>"CPF invalido"));
+            return response()->json(["error" => "CPF invalido"]);
         }
 
         $validator = Validator::make($request->all(), [
-            'name'      => ['required', 'string', 'max:255'],
-            'phone'     => ['required', 'string', 'max:11'],
-            'email'     => ['required', 'string', 'email', 'max:255','unique:users'],
-            'password'  => ['required', 'string', 'min:4'],
-        ],[
-            'name.required'         => 'Nome do usuário obrigatorio',
-            'name.max'              => 'Caractere maximo para o nome foi ultrapassado',
-            'phone.required'        => 'Telefone é obrigatorio',
-            'phone.max'             => 'Caractere maximo para o telefone foi ultrapassado',
-            'email.required'        => 'Email obrigatorio',
-            'email.unique'          => 'Esse email foi cadastrado para outro usuário',
-            'email.max'             => 'Caractere maximo para o email foi ultrapassado',
-            'email.email'           => 'Email invalido',
-            'password.required'     => 'Senha obrigatória',
-            'password.min'          => 'É necessario mais caracteres para senha',
-           ]
+            'name' => ['required', 'string', 'max:255'],
+            'phone' => ['required', 'string', 'max:11'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:4']
+        ], [
+            'name.required' => 'Nome do usuário obrigatorio',
+            'name.max' => 'Caractere maximo para o nome foi ultrapassado',
+            'phone.required' => 'Telefone é obrigatorio',
+            'phone.max' => 'Caractere maximo para o telefone foi ultrapassado',
+            'email.required' => 'Email obrigatorio',
+            'email.unique' => 'Esse email foi cadastrado para outro usuário',
+            'email.max' => 'Caractere maximo para o email foi ultrapassado',
+            'email.email' => 'Email invalido',
+            'password.required' => 'Senha obrigatória',
+            'password.min' => 'É necessario mais caracteres para senha'
+        ]
         );
 
         if ($validator->fails()) {
-            return response()->json(array("error"=>$validator->errors()->first()));
+            return response()->json(["error" => $validator->errors()->first()]);
         }
 
-        if (User::where('email','=',$request->email)->first()) {
-            return response()->json(array("error" => "Esse email foi cadastrado para outro usuário"));
+        if (User::where('email', '=', $request->email)->first()) {
+            return response()->json(["error" => "Esse email foi cadastrado para outro usuário"]);
         }
 
-        if (User::where('cpf','=',$cpf_cnpj)->first()) {
-            return response()->json(array("error" => "Esse cpf foi cadastrado para outro usuário"));
+        if (User::where('cpf', '=', $cpf_cnpj)->first()) {
+            return response()->json(["error" => "Esse cpf foi cadastrado para outro usuário"]);
         }
 
         if ($user = User::create([
-            'name'      => $request->name,
-            'email'     => $request->email,
-            'cpf'       => $cpf_cnpj,
-            'phone'     => $request->phone,
-            'password'  => Hash::make($request->password),
+            'name' => $request->name,
+            'email' => $request->email,
+            'cpf' => $cpf_cnpj,
+            'phone' => $request->phone,
+            'password' => Hash::make($request->password)
         ])) {
             $user->save();
             $user->token = $user->createToken($request->email)->accessToken;
-            return response()->json(array("success" => "Usuário registrado com sucesso", "user" => $user));
-        } 
-        return response()->json(array("error"=>"Erro ao registrar o usuário"));
+            return response()->json(["success" => "Usuário registrado com sucesso", "user" => $user]);
+        }
+        return response()->json(["error" => "Erro ao registrar o usuário"]);
     }
 
+    /**
+     * @param Request $request
+     */
     protected function get(Request $request)
     {
         if (!$user = Auth::user()) {
-            return response()->json(array("error" => "Usuário não foi autenticado"));
+            return response()->json(["error" => "Usuário não foi autenticado"]);
         }
-        $user_obj       = new User();
+        $user_obj = new User();
         $user_obj->name = $request->name;
-        $user_obj->cpf  = $request->cpf;
+        $user_obj->cpf = $request->cpf;
         return response()->json($user_obj->get());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,36 +11,34 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::post('/login',      'UserController@login');
-Route::post('/register',   'UserController@register');
+Route::post('/login', 'UserController@login');
+Route::post('/register', 'UserController@register');
 
-Route::group(['middleware' => 'auth:api'], function() {
-    Route::prefix('users')->group(function($id) {
-        Route::get('/' , 'UserController@get');
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::prefix('users')->group(function ($id) {
+        Route::get('/', 'UserController@get');
     });
 
-    Route::prefix('account')->group(function($id) {
-        Route::post('/' , 'AccountController@new');
-        Route::get('/{id}' , 'AccountController@get');
+    Route::prefix('account')->group(function ($id) {
+        Route::post('/', 'AccountController@new');
+        Route::get('/{id}', 'AccountController@get');
     });
 
-    Route::prefix('transaction')->group(function($id) {
-        Route::post('/deposit'       , 'TransactionController@deposit');
-        Route::post('/cell_recharge' , 'TransactionController@cellRecharge');
-        Route::post('/bill_payment'  , 'TransactionController@billPayment');
-        Route::post('/transfer'      , 'TransactionController@transfer');
-        Route::post('/card_pay'      , 'TransactionController@cardPay');
+    Route::prefix('transaction')->group(function ($id) {
+        Route::post('/deposit', 'TransactionController@deposit');
+        Route::post('/cell_recharge', 'TransactionController@cellRecharge');
+        Route::post('/bill_payment', 'TransactionController@billPayment');
+        Route::post('/transfer', 'TransactionController@transfer');
+        Route::post('/card_pay', 'TransactionController@cardPay');
     });
 
-    Route::prefix('card')->group(function() {
-        Route::post('/new_card' , 'CardController@newCard');
+    Route::prefix('card')->group(function () {
+        Route::post('/new_card', 'CardController@newCard');
 
-        Route::prefix('card_transaction')->group(function() {
-            Route::post('/' , 'CardTransactionController@new');
+        Route::prefix('card_transaction')->group(function () {
+            Route::post('/', 'CardTransactionController@new');
         });
     });
 });
-
-
